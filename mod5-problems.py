086a112f8e3d4270d8e56538a3646e61d2209ab6
Scott Jennings <scoejen@gmail.com>
2025-02-09 08:05:09 -0800

    Update mod5-problems.py

--- mod5-problems.py
+++ mod5-problems.py
@@ -1,4 +1,4 @@
-# Write your code here :-)
+# MODULE 5 PRACTICE PROBLEMS
 
 #1. Prompt the user for a positive integer n.
 #   Use a while loop to sum all the integers from 1 up to n. Print the final sum.
