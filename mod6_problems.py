5a67e3aadeb07bac8d572b338ed7c89c669bee17
Scott Jennings <scoejen@gmail.com>
2025-02-15 01:02:41 -0800

    NODULE 6 PRACTICE PROBLEMS

---
+++ mod6_problems.py
@@ -0,0 +1,54 @@
+# MODULE 6 PRACTICE PROBLEMS
+
+#1. Given a list of numbers, write code to iterate through the list and calculate the sum of all even numbers.
+#   Print the resulting sum.
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# sum = 0
+# for number in numbers:
+#    if number % 2 == 0:
+#        sum += number
+# print(sum)
+
+#2. With a list of strings provided, write code that counts how many times the word "Olympic" appears in the list,
+#   and then print the count.
+# strings = ["Olympic", "Batman", "Superman", "Wonderwoman", "Olympic", "Flash", "Aquaman"]
+# counter = 0
+# for string in strings:
+#    if string == "Olympic":
+#        counter += 1
+# print(counter)
+
+#3. Given a list of strings, write code to create a new list that includes only the strings longer than three
+#   characters. Print the resulting filtered list.
+#strings = ["Iron Man", "Up", "Two", "High", "Spider-Man"]
+#new_list = []
+#for i in strings:
+#    if len(i) > 3:
+#        new_list.append(i)
+#print(new_list)
+
+#4. For a list of integers, write code that counts how many numbers are positive and how many are negative,
+#   then print both counts.
+#numbers = [3, 2, 1, -1, -2, -3]
+#counter = 0
+#for number in numbers:
+#    if number <= 0:
+#        counter += 1
+#        print("There are", negative, " negative numbers.")
+#    else:
+#        counter += 1
+#        print("There are", counter, "positive numbers.")
+
+#5. For a list of integers, use a loop to build a new list where each element is the square of the corresponding
+#   element in the original list. Print the new list.
+#my_list = [20, 40, 60, 80, 100]
+#new_list = []
+
+#for number in my_list:
+#    if number > 0:
+#        number *= number
+#        new_list.append(number)
+#
+#print(new_list)
+
+
